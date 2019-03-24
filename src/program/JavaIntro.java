176ecb25c1dd31211742ce0517fa37e39d98390f
Yu Hao <yuhao88921@gmail.com>
2019-03-25 00:19:51 +0800

    add JavaIntor

---
+++ src/program/JavaIntro.java
@@ -0,0 +1,14 @@
+package program;
+
+public class JavaIntro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("聖母峰有多高?");
+		System.out.print(8848);
+		System.out.print("公尺");
+	}
+
+	
+
+}
