4030d3c789a84b075269618c059b1e90a0e0e288
Yu Hao <yuhao88921@gmail.com>
2019-03-25 23:54:40 +0800

    commit amest

---
+++ src/program/Casting.java
@@ -0,0 +1,16 @@
+package program;
+
+public class Casting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1;
+	      double d = 11.1;
+	      double sum1 = i + d;
+	      int sum2 =(int)(i+d);
+	      System.out.println(sum1);
+	      System.out.println(sum2);
+			
+	}
+
+}
