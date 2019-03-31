7a5f0e4019caf3ac42374dc31544846a8a87b93f
yuhaosir <49100508+yuhaosir@users.noreply.github.com>
2019-04-01 02:05:43 +0800

    add remote chg2

--- src/program/AssignOP.java
+++ src/program/AssignOP.java
@@ -2,7 +2,7 @@ package program;
 
 public class AssignOP {
 
-	public static void main(String[] args) {
+	public static void main(String2[] args) {
 		// TODO Auto-generated method stub
 		int i = 1;
 		i += 2;
@@ -11,6 +11,7 @@ public class AssignOP {
 		System.out.println(i);
 		System.out.println(s);
 		//this is remote change
+		//this is remote change2
 
 	}
 
