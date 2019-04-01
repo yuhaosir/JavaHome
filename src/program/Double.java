c7d8f3847a0632a881d6c07c3ba35da87bcba14e
Yu Hao <yuhao88921@gmail.com>
2019-04-01 22:30:51 +0800

    add 9x9

---
+++ src/program/Double.java
@@ -0,0 +1,19 @@
+package program;
+
+public class Double {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			for(int y=1;y<=9;y++ ) {
+		
+			for(int i=1;i<=9;i++) {
+			System.out.print(i+"x"+y+"="+i*y+"\t");
+			
+			
+			}
+			System.out.println();
+		}
+ 
+	}
+
+}
